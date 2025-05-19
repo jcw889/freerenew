59d3dca65dbdf14e47df108e2adb62569553f5b6
jcw889 <104991572+jcw889@users.noreply.github.com>
2025-05-19 20:49:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import requests
 #   TELEGRAM_BOT_TOKEN：你的Bot Token
 #   TELEGRAM_CHAT_ID：你的聊天ID
 
-BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")   # TG机器人token                                        
-CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")       # 用户ID或频道、群ID                                               
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")   # TG机器人token 
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")       # 用户ID或频道、群ID                                                 
 
 # 其他凭证（环境变量）
 USERNAME = os.getenv("FC_USERNAME")      # 用户名                        
